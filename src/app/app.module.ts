5b9d27e02a70a28311c5e038190e090b90ea2d7d
Beto Martinez <betoml5@hotmail.com>
2020-04-20 21:24:43 -0500

    Genere los componentes necesarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AlumnosComponent } from './components/alumnos/alumnos.component';
+import { CarrerasComponent } from './components/carreras/carreras.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AlumnosComponent,
+    CarrerasComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule
