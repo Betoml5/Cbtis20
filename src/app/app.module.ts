b701dde1bea57bf56f14fcf46df27417cde3995c
Beto Martinez <betoml5>
2020-04-24 00:49:04 -0500

    Agrege la seccion main solo para maquetar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { CarrerasComponent } from './components/carreras/carreras.component';
 import { ContactoComponent } from './components/contacto/contacto.component';
 import { appRoutingProviders, routing } from './routes';
 import { IniciarSesionComponent } from './components/iniciar-sesion/iniciar-sesion.component';
+import { GruposComponent } from './components/grupos/grupos.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { IniciarSesionComponent } from './components/iniciar-sesion/iniciar-sesi
     AlumnosComponent,
     CarrerasComponent,
     ContactoComponent,
-    IniciarSesionComponent
+    IniciarSesionComponent,
+    GruposComponent
   ],
   imports: [
     BrowserModule,
