f9808f830b4ba16c75c654d4b9f4771c7f5c58df
Beto Martinez <betoml5>
2020-05-15 00:47:10 -0500

    Backend working now, we can insert Post, without image, that feature will be add soon :S

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -1,64 +1,63 @@
-
-
 const Post = require('../models/post')
-const fs = require('fs')
-const path = require('path')
 
 const controller = {
+  test: function (req, res) {
+    return res.status(200).send({
+      message: "Soy el metodo test",
+    });
+  },
 
+  savePost: function (req, res) {
+    const post = new Post();
 
-    test: function(req, res){
-        return res.status(200).send({
-            message: 'Soy el metodo test'
-        })
-    },
+    const params = req.body;
+    post.title = params.title;
+    post.content = params.content;
+    post.date = params.date;
+    post.category = params.category;
+    post.author = params.author;
 
-    savePost: function(req, res){
-        const post = new Post();
+    post.save((err, postStored) => {
+      if (err)
+        return res.status(500).send({ message: "Error al guardar el post" });
 
-        const params = req.body
-        post.title = params.title
-        post.date = params.date
-        post.category = params.category
-        post.image = null
-        post.author = params.author
+      if (!postStored)
+        return res.status(404).send({ message: "No se ha podido guardar el post" });
 
-        post.save( (err, postStored) =>{
-            if (err) return res.status(500).send({message: 'Error al guardar el post'}) 
-            
-            if(!postStored) return res.status(404).send({message: 'No se ha podido guardar el post'})
+      return res.status(200).send({ post: postStored });
+    });
+  },
 
-            return res.status(200).send({post: postStored})
-            
-        })
+  getPost: function (req, res) {
+    const postId = req.params.postId;
 
-    },
+    if (postId === null)
+      return res.status(404).send({ message: "El post no existe" });
 
-    getPost: function(req, res){
-        const postId = req.params.postId
+    Post.findById(postId, (err, post) => {
+      if (err)
+        return res.status(500).send({ message: "Error al devovler los datos" });
 
-        if(postId === null) return res.status(404).send({message: 'El post no existe'})
+      return res.status(200).send({
+        post,
+      });
+    });
+  },
 
-        Post.findById(postId, (err, post) => {
-            if(err) return res.status(500).send({message: 'Error al devovler los datos'})
+  getPosts: function (req, res) {
+    Post.find({})
+      .sort("-date")
+      .exec((err, posts) => {
+        if (err)
+          return res
+            .status(500)
+            .send({ message: "Error al devolver los datos" });
 
-            return res.status(200).send({
-                post
-            })
-        })
-    },
+        if (!posts)
+          return res.status(404).send({ message: "No hay post por mostrar" });
 
-    getPosts: function(req, res){
-        Post.find({}).sort('-date').exec((err, post) =>{
-            if(err) return res.status(500).send({message: 'Error al devolver los datos'})
-
-            if(!posts) return res.status(404).send({message:'No hay post por mostrar'})
-
-            return res.status(200).send({posts})
-        })
-    }
-
-
-}
-
+        return res.status(200).send({ posts });
+      });
+  }
+};
 module.exports = controller;
--- backend/models/post.js
+++ backend/models/post.js
@@ -3,12 +3,13 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
-const ProjectSchema = Schema({
+const Post = new Schema({
     title: String,
-    date: Date,
+    content: String,
+    date: Number,
     category: String,
-    image: String,
     author: String,
+   
 });
 
-module.exports = mongoose.model('post', ProjectSchema)
+module.exports = mongoose.model('post', Post)
